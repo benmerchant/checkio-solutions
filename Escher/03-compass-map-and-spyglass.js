76a8320fcf234c5f6ac0cf9cfd7bcfb5ce50edf5
Ben Merchant <benjaminmerchant@gmail.com>
2019-04-01 10:07:16 -0400

    Escher-03: working perfectly. Quick refactor

--- Escher/03-compass-map-and-spyglass.js
+++ Escher/03-compass-map-and-spyglass.js
@@ -11,74 +11,75 @@
   *
 */
 function navigation(seaside) {
-
-    getLocations(seaside,(itemLocs) => {
-      getDistancesToRespectiveObjects(itemLocs,(distObj) => {
-        byThePowerOfPythagorasProbably(distObj, (finalAnswer) => {
-          console.log(finalAnswer);
-          return finalAnswer;
-        });
+  let outgoingFinalAnswer = 0;
+  getLocations(seaside,(itemLocs) => {
+    getDistancesToRespectiveObjects(itemLocs,(distObj) => {
+      getAnswerNoPythagorasRequired(distObj, (finalAnswer) => {
+        console.log(finalAnswer);
+        outgoingFinalAnswer = finalAnswer;
       });
     });
+  });
+  return outgoingFinalAnswer;
 
-
-    function getLocations(challengeInput,callback){
-      const locations = {}; // get x,y coordinates
-      for(let i=0;i<challengeInput.length;i++){
-        for(let j=0;j<challengeInput[i].length;j++){
-          switch(challengeInput[i][j]){
-            case 'Y':
-              locations.Y = {y:i,x:j};
-              break;
-            case 'M':
-              locations.M = {y:i,x:j};
-              break;
-            case 'C':
-              locations.C = {y:i,x:j};
-              break;
-            case 'S':
-              locations.S = {y:i,x:j};
-              break;
-          }
+  function getLocations(challengeInput,callback){
+    const locations = {}; // get x,y coordinates
+    for(let i=0;i<challengeInput.length;i++){
+      for(let j=0;j<challengeInput[i].length;j++){
+        switch(challengeInput[i][j]){
+          case 'Y':
+            locations.Y = {y:i,x:j};
+            break;
+          case 'M':
+            locations.M = {y:i,x:j};
+            break;
+          case 'C':
+            locations.C = {y:i,x:j};
+            break;
+          case 'S':
+            locations.S = {y:i,x:j};
+            break;
         }
       }
-      callback(locations);
-    };
-    function getDistancesToRespectiveObjects(itemLocObj,callback){
-      const distObject = {};
-      for(let key in itemLocObj){ // find horizontal or vertical distances
-        if(key==='Y') continue;
-        else {
-          distObject[`${key}_Dist`] = {};
-          // see if perfect horizontal
-          if(itemLocObj.Y.y === itemLocObj[key].y){
-            distObject[`${key}_Dist`].theDist = Math.abs(itemLocObj.Y.x - itemLocObj[key].x);
-          } else
-          // see if perfect vertical
-          if(itemLocObj.Y.x === itemLocObj[key].x){
-            distObject[`${key}_Dist`].theDist = Math.abs(itemLocObj.Y.y - itemLocObj[key].y);
-          } else {
-            distObject[`${key}_Dist`].yDist = Math.abs(itemLocObj.Y.y - itemLocObj[key].y);
-            distObject[`${key}_Dist`].xDist = Math.abs(itemLocObj.Y.x - itemLocObj[key].x);
-          }
+    }
+    callback(locations);
+  };
+  function getDistancesToRespectiveObjects(itemLocObj,callback){
+    const distObject = {};
+    for(let key in itemLocObj){ // find horizontal or vertical distances
+      if(key==='Y') continue;
+      else {
+        distObject[`${key}_Dist`] = {};
+        // see if perfect horizontal
+        if(itemLocObj.Y.y === itemLocObj[key].y){
+          distObject[`${key}_Dist`].theDist = Math.abs(itemLocObj.Y.x - itemLocObj[key].x);
+        } else
+        // see if perfect vertical
+        if(itemLocObj.Y.x === itemLocObj[key].x){
+          distObject[`${key}_Dist`].theDist = Math.abs(itemLocObj.Y.y - itemLocObj[key].y);
+        } else {
+          distObject[`${key}_Dist`].yDist = Math.abs(itemLocObj.Y.y - itemLocObj[key].y);
+          distObject[`${key}_Dist`].xDist = Math.abs(itemLocObj.Y.x - itemLocObj[key].x);
         }
       }
-      callback(distObject);
-    };
-    function byThePowerOfPythagorasProbably(distObject,callback){
-      let integerForAnswerReturner = 0;
-      // i say probably b/c we might not need him
-      // in the case they're all straight, lets end this now
-      for(let key in distObject){
-        // if their dist has 2-componenents, need Pythag
-        if (typeof(Object.keys(distObject[key]).length)==='number' && Object.keys(distObject[key]).length > 1) {
-          integerForAnswerReturner += Math.sqrt(distObject[key].yDist ^ 2 + distObject[key].xDist ^ 2);
-
-          break;
-        } else { integerForAnswerReturner += distObject[key].theDist; }
-      }
-      callback(integerForAnswerReturner);
-    };
+    }
+    callback(distObject);
+  };
+  function getAnswerNoPythagorasRequired(distObject,callback){
+    let intOut = 0;
+    // i say probably b/c we might not need him
+    // in the case they're all straight, lets end this now
+    console.log(distObject);
+    for(let key in distObject){
+      // if their dist has 2-componenents, need Pythag
+      if (typeof(Object.keys(distObject[key]).length)==='number' && Object.keys(distObject[key]).length > 1) {
+        // the smaller of the distances will be the distance you would need to travel
+        // even combining diagonal and straight
+        (distObject[key].yDist > distObject[key].xDist) ? intOut += distObject[key].yDist : intOut += distObject[key].xDist;
+      } else { intOut += distObject[key].theDist; }
+    }
+    callback(intOut);
+  };
 };
 ////// END SOLUTION //////
 
